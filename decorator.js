76081d762252a5a323a3c81340b1835466a117d0
cathyatseneca <cathyatseneca@gmail.com>
2017-01-30 12:41:56 -0500

    updated code

--- decorator.js
+++ decorator.js
@@ -1,15 +1,33 @@
-var Label =function(text){
+var Label = function(text,x,y){
 	var that = {};
 	var theText_=text || "";
-	var hasBorder = true;
-	var isFilled = false;
-	var isBolded = false;
-	var isItalics = false;
-	var fillColour = color(255,255,255); //white
-	var borderColour = color(0,0,0);  //black
+	var x_=x || 100;
+	var y_=y || 100;
+	var hasBorder_ = true;
+	var isFilled_ = false;
+	var isBolded_ = false;
+	var isItalics_ = false;
+	var fillColour_ = color(255,255,255); //white
+	var borderColour_ = color(0,0,0);  //black
 
-	var draw() = function{
-		
+	that.draw = function(){
+		if(hasBorder_){
+			noFill();
+			rect(x_-50,y_-50,100,100);			
+		}
+		fill(0,0,255);
+		text(theText_,x_,y_);
 	}
+	return that;
+};
 
+var l;
+function setup(){
+	createCanvas(500,500);
+	l = Label("the Label", 100,100);
 }
+function draw(){
+	background(255,255,255);
+	l.draw();
+}
+	
