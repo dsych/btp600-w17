9f08fa60cda6b8e90c79d3918a933a9770479201
cathyatseneca <cathyatseneca@gmail.com>
2017-02-01 11:47:48 -0500

    updated p5.js and p5.dom.js, added checkbox to p5js version

--- decorator.js
+++ decorator.js
@@ -4,27 +4,51 @@ var Label = function(labelText,x,y){
 	var x_=x || 100;
 	var y_=y || 100;
 	var hasBorder_ = true;
-	var isFilled_ = false;
+	var isFilled_ = true;
 	var isBolded_ = false;
 	var isItalics_ = false;
-	var fillColour_ = color(255,255,255); //white
+	var fillColour_ = color(0,255,255); //white
 	var borderColour_ = color(0,0,0);  //black
 
 	that.draw = function(){
 		if(hasBorder_){
+			strokeWeight(2);
+			stroke(borderColour_);
 			noFill();
-			rect(x_-50,y_-50,100,100);			
+			rect(x_-30,y_-10,100,20);			
 		}
-		fill(0,0,255);
+		if(isFilled_){
+			 noStroke();   //no border
+			 fill(fillColour_);
+			 rect(x_-30,y_-10,100,20);						
+		}
+		if(isItalics_){
+
+		}
+		if(isBolded_){
+
+		}
+		stroke(0,0,0);
+		fill(0,0,0);
 		text(theText_,x_,y_);
+	}
+	that.changeBorder=function(checked){
+		hasBorder_=checked;
 	}
 	return that;
 };
 
 var l;
+var borderCheckBox
 function setup(){
+	borderCheckBox = createCheckbox("border", true);
+  	borderCheckBox.changed(checkBorder);
 	createCanvas(500,500);
 	l = Label("the Label", 100,100);
+
+}
+function checkBorder(){
+	l.changeBorder(borderCheckBox.checked());
 }
 function draw(){
 	background(255,255,255);
